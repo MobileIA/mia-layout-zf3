b798e76c3094350bda07fbd182d7ea0544cbb526
Matias Camiletti <matias.camiletti@gmail.com>
2017-12-09 14:03:54 -0300

    Primer commit.

---
+++ config/module.config.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @license   http://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
+ * @copyright Copyright (c) 2014 Zend Technologies USA Inc. (http://www.zend.com)
+ */
+namespace MIALayout;
+
+use Zend\ServiceManager\Factory\InvokableFactory;
+
+return array(
+    'view_helpers' => [
+        /*'aliases' => [
+            'layoutLte' => View\Helper\LayoutLte::class,
+            'lteForm' => View\Helper\LteForm::class,
+        ],
+        'factories' => [
+            View\Helper\LayoutLte::class => View\Helper\LayoutLteFactory::class,
+            View\Helper\LteForm::class => InvokableFactory::class,
+        ],*/
+    ],
+    /*'mia_layout_elite' => [
+        'title' => 'AdminElite',
+        'title_part_one' => 'Admin',
+        'title_part_two' => 'LTE',
+    ]*/
+);
